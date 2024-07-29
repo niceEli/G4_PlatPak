ed766e065b7d5dee0574984f7b5ceeb0997ebb7b
Eli <78963198+niceEli@users.noreply.github.com>
2024-07-29 19:58:25 -0400

    feat: M/s

--- scripts/PlayerStats.gd
+++ scripts/PlayerStats.gd
@@ -10,4 +10,4 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	set_text("FPS:      {fps}\nX:            {x}\nY:            {y}\nZ:            {z}\nSPEED: {speed}".format({"x": str(round(player.global_position.x * 100) / 100), "y": str(round(player.global_position.y * 100) / 100), "z": str(round(player.global_position.z * 100) / 100), "fps": str(Engine.get_frames_per_second()), "speed": str(round(player.get_real_velocity().length()*100)/100)}))
+	set_text("FPS:      {fps}\nX:            {x}\nY:            {y}\nZ:            {z}\nSPEED: {speed} M/S".format({"x": str(round(player.global_position.x * 100) / 100), "y": str(round(player.global_position.y * 100) / 100), "z": str(round(player.global_position.z * 100) / 100), "fps": str(Engine.get_frames_per_second()), "speed": str(round(player.get_real_velocity().length()*100)/100)}))
